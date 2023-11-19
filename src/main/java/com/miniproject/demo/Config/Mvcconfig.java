beea5d58576b898df6feacbb5a82b22138915eba
unknown <hd1@somaiya.edu>
2023-11-19 19:41:32 +0530

    reversed

--- src/main/java/com/miniproject/demo/Config/Mvcconfig.java
+++ src/main/java/com/miniproject/demo/Config/Mvcconfig.java
@@ -21,6 +21,11 @@ public class Mvcconfig implements WebMvcConfigurer {
         registry.addViewController("/Viewer/index").setViewName("Viewer page/index");
         registry.addViewController("/Viewer/upcoming").setViewName("Viewer page/upcoming");
         registry.addViewController("/Viewer/pt").setViewName("Viewer page/pt");
+        registry.addViewController("/Scorer/cricket").setViewName("Scorer page/cricket");
+        registry.addViewController("/Scorer/football").setViewName("Scorer page/football");
+        registry.addViewController("/Scorer/batsecond").setViewName("Scorer page/battingSecondInnings");
+        registry.addViewController("/Scorer/bowlfirst").setViewName("Scorer page/firstInningsBowler");
+        registry.addViewController("/Scorer/bowlsecond").setViewName("Scorer page/secondInningsBowler");
     };
 
     @Bean
